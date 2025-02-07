c0d213a580d84ceaa0857899d31043a4b1741495
NavyeshSorathiya <sorathiyanavyesh2003@gmail.com>
2025-02-07 08:26:59 +0530

    control stamt. + stringmani.

---
+++ Python Fundamentals/String Manipulation/Q-9.1.py
@@ -0,0 +1,12 @@
+str  = "Tops Technologies123"
+
+print(str[0:7])
+print(str[0:20:2])
+print(str[2:10])
+print(str[::3])
+print(str[::-1])
+
+print(str[-7:])
+print(str[-10:-5])
+print(str[::-1])
+print(str[::])
---
+++ Python Fundamentals/String Manipulation/Q-9.2.py
@@ -0,0 +1,17 @@
+str = "Tops Technologies123"
+
+print(str.islower())
+print(str.isupper())
+print(str.isalnum())
+print(str.isalpha())
+print(str.isdigit())
+print(str.isnumeric())
+
+
+print(str.upper())
+print(str.lower())
+print(str.title())
+print(str.capitalize())
+print(str.count("o"))
+print(str.replace("e", "a"))
+print(str.index("o"))
